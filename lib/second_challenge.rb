76a53f700bd124239b5a3e9ca49e37956761c991
K e n V <kenneth_valdivia@yahoo.com>
2019-12-26 05:15:05 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,5 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {:patriarch, :matriach, :hero, :hero_freinds}
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+ 
 
   
 end
